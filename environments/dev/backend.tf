df3ee7189aa000da57e916f8a7b52d184520b392
mdsahil7 <mohammed.sahil@niveussolutions.com>
2023-01-21 12:32:49 +0530

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "prj-shared-cicd-375013-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="prj-shared-cicd-375013"
